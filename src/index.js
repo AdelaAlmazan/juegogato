78e01657e679dd3d379d156c2613d77ea5b6731c
AdelaAlmazan <lopezadela606@gmail.com>
2022-04-30 16:00:16 -0600

    agregar metodo jumpTo

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
@@ -72,6 +73,14 @@ class Game extends React.Component {
       }]),
       xIsNext: !this.state.xIsNext,
     });
+  }
+
+
+  jumpTo(step){
+      this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0
+      });
   }
   
   render() {
